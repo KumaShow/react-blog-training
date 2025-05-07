222f1b0bd1720a9422acb19cfbfa8a070ebc68cd
Benson <kumadth@gmail.com>
2025-05-07 19:47:47 +0800

    feat: 移除 Navbar 元件的背景顏色樣式

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Link} from "react-router-dom"
 
 export default function Navbar () {
   return (
-    <nav className="bg-amber-300 w-screen text-center">
+    <nav className=" text-center">
       <Link className="px-4 py-6 inline-block" to="/">首頁</Link>
       <Link className="px-4 py-6 inline-block" to="/blog">部落格</Link>
     </nav>
