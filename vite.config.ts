6d8fc988c44045c1166580ddb08850495d9ddd88
Benson <kumadth@gmail.com>
2025-05-08 07:21:10 +0800

    fix: 修正 Vite 配置中的 base 路徑拼寫錯誤

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/react-blog-traning/',
+  base: '/react-blog-training/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
