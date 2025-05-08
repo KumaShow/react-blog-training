415769e1916c95fe337cf028ac1587f93f5366ec
Benson <kumadth@gmail.com>
2025-05-09 00:15:21 +0800

    fix: 修正 Vite 設定以支持不同環境的base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/react-blog-training/',
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command }) => {
+  const productionBase = '/react-blog-training/';
+
+  return {
+    plugins: [react(), tailwindcss()],
+    base: command === 'serve' ? '/' : productionBase,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-})
+  };
+});
