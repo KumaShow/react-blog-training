fc341021ebecc3622102dc50998ebf49279df100
Benson <kumadth@gmail.com>
2025-05-12 00:42:21 +0800

    fix: 移除註解，並更新　參數型別

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -7,43 +7,8 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 export default function Blog() {
 
-  // 假資料：部落格文章列表
-  // const blogPostList: BlogPost[] = [
-  //   {
-  //     id: 0,
-  //     isNew: true,
-  //     isHot: false,
-  //     date: '2024/10/21',
-  //     title: '自學前端不用怕：從零開始的三大關鍵',
-  //     tags: ['前端開發', '職涯成長'],
-  //     description: '嗨，我是 Alyse，一名前端工程師兼職涯諮詢師。一直以來，我都很喜歡在部落格分享學習與工作心得，也常有讀者問：「我想轉職/自學前端，該從哪裡開始？」其實自學的過程既自由又具挑戰性。我整理了三大關鍵，幫助你在短期內建立紮實基礎，並快速累積實戰經驗。希望能替你的前端之路帶來一些啟發與動力！',
-  //     imgUrl: 'https://images.unsplash.com/photo-1456615074700-1dc12aa7364d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  //   },
-  //   {
-  //     id: 1,
-  //     isNew: false,
-  //     isHot: false,
-  //     date: '2024/10/11',
-  //     title: 'React 入門不再迷惘：三步驟帶你上手核心概念',
-  //     tags: ['React', '入門基礎'],
-  //     description: '你是否對 React 感到好奇，卻苦惱於該如何正式啟動學習之路？身為前端工程師兼職涯諮詢師，我常遇到同學問我：「React 到底該怎麼入門？」為了幫大家減少摸索的時間，我整理出三個循序漸進的關鍵步驟，帶你更輕鬆地掌握 React 核心概念，真正把理論應用在實際專案中。',
-  //     imgUrl: 'https://images.unsplash.com/photo-1456615074700-1dc12aa7364d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  //   },
-  //   {
-  //     id: 2,
-  //     isNew: false,
-  //     isHot: false,
-  //     date: '2024/09/07',
-  //     title: '前端作品集打造指南：讓你的專案成為履歷亮點',
-  //     tags: ['作品集', '求職攻略'],
-  //     description: '對正在求職的前端工程師而言，作品集往往是第一個「說話」的利器。當面試官瀏覽你的網頁作品時，能夠快速了解你的程式邏輯、設計感以及解決問題的思路。我在協助多位同學優化履歷與作品集的過程中，總結出一些關鍵要素，分享給正在打造、升級作品集的你。',
-  //     imgUrl: 'https://images.unsplash.com/photo-1746240922260-efbea47dc532?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  //   },
-  // ];
-
   // 假資料：最新文章
-  const lastestBlogPost: BlogPost = blogPostList.filter(blogPost => blogPost.isNew)[0];
-
+  const lastestBlogPost: BlogPost = blogPostList.filter((blogPost: BlogPost) => blogPost.isNew)[0];
 
   return (
     <div>
@@ -102,7 +67,7 @@ export default function Blog() {
 
           <ul className="md:px- py-12 space-y-8 md:space-y-0 md:grid md:grid-cols-2 md:gap-6 lg:grid-cols-3">
             {
-              blogPostList.filter(blogPost => !blogPost.isNew).map((blogPost: BlogPost) => {
+              blogPostList.filter((blogPost: BlogPost) => !blogPost.isNew).map((blogPost: BlogPost) => {
                 return (
                   <li key={blogPost.id}>
                     <Card {...blogPost} />
