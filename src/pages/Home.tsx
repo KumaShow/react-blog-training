2cd89cea7840ecc2e70194e154bcad63fb875ad6
Benson <kumadth@gmail.com>
2025-05-12 00:21:46 +0800

    feat: 更新首頁內容，新增 Banner 元件及個人介紹

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,18 @@
+import Banner from "@/components/Banner"
+
 export default function Home() {
   return (
-    <div>這是首頁</div>
+    <div>
+      <section>
+        <Banner title="Alyse Wang" subTitle="前端工程師 & 職涯諮詢師" />
+
+        <div>
+          <p>
+            嗨，我是 Alyse 一名深耕前端技術的工程師<br />
+            擅長 React、Vue 等框架，同時熱愛為轉職與新手工程師提供職涯指導。邀請你與我一起，開啟更具潛能的程式與職涯之旅！
+          </p>
+        </div>
+      </section>
+    </div>
   )
 }
