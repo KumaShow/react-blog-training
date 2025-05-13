b872ea98a49b3fee9710ad536a244c6679e1b50f
Benson <kumadth@gmail.com>
2025-05-14 00:14:52 +0800

    fix: 調整 Card 元件的屬性定義，新增 aosDelay 支援

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,8 +5,12 @@ interface Badge {
   label: string;
 }
 
-const Card = (blogPost: BlogPost) => {
-  const { title, date, tags, description, imgUrl, isNew, isHot } = blogPost;
+interface CardProps extends BlogPost {
+  aosDelay?: number;
+}
+
+const Card = (props: CardProps) => {
+  const { title, date, tags, description, imgUrl, isNew, isHot, aosDelay } = props;
 
   // 最新文章、人氣文章的條件 Map
   const additionalBadges: Badge[] = [
@@ -14,11 +18,16 @@ const Card = (blogPost: BlogPost) => {
     { condition: !!isNew, label: "人氣文章" },
   ];
   const renderBadges = additionalBadges
-    .filter(badge => badge.condition)
-    .map(badge => badge.label)
+    .filter((badge: Badge) => badge.condition)
+    .map((badge: Badge) => badge.label)
 
   return (
-    <>
+    <div 
+      data-aos="fade-up" 
+      data-aos-delay={aosDelay} 
+      data-aos-duration="500" 
+      className="hover:shadow-lg hover:-translate-y-2.5 transition-all duration-300 bg-white p-6"
+    >
       <img className="mb-4 max-h-[250px] w-full object-cover" src={imgUrl} alt={title} />
 
       <p className="mb-1">{date}</p>
@@ -35,7 +44,7 @@ const Card = (blogPost: BlogPost) => {
 
         {/* 渲染 Badge */}
         {
-          renderBadges.map((badgeText, index) => 
+          renderBadges.map((badgeText, index) =>
             <span key={index} className="bg-primary text-white py-1.5 px-3 ms-2 rounded-4xl font-bold">
               {badgeText}
             </span>
@@ -52,7 +61,7 @@ const Card = (blogPost: BlogPost) => {
       <div>
         <a href="#" className="inline-block text-black py-2 px-3 border-black border-1 rounded-4xl">閱讀內文</a>
       </div>
-    </>
+    </div>
   )
 };
 
