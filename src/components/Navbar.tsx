a0b315ef0cbc1ddee779cc2155c3043f27fa6182
Benson <kumadth@gmail.com>
2025-05-14 00:15:03 +0800

    fix: 調整 Navbar 元件的連結樣式，新增字體粗體設定

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom"
 export default function Navbar() {
   return (
     <nav className=" text-center">
-      <Link className="px-4 py-6 inline-block" to="/">首頁</Link>
-      <Link className="px-4 py-6 inline-block" to="/blog">部落格</Link>
+      <Link className="px-4 py-6 inline-block text-xl font-bold" to="/">首頁</Link>
+      <Link className="px-4 py-6 inline-block text-xl font-bold" to="/blog">部落格</Link>
     </nav>
   )
 }
