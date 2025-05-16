9883d39b9fa83acdbb08419f91144888975aa065
Benson <kumadth@gmail.com>
2025-05-16 23:00:39 +0800

    feat: 將 Navbar 元件中的 Link 替換為 NavLink，增強路由功能

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,20 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 export default function Navbar() {
   return (
     <nav className=" text-center">
-      <Link className="px-4 py-6 inline-block text-xl font-bold" to="/">首頁</Link>
-      <Link className="px-4 py-6 inline-block text-xl font-bold" to="/blog">部落格</Link>
+      <NavLink
+        to="/"
+        end
+        className={({ isActive }: { isActive: boolean }) =>
+          `px-4 py-6 inline-block text-xl font-bold ${isActive ? " text-primary border-b border-primary" : ""}`
+        }
+      >
+        首頁
+      </NavLink>
+      <NavLink className={({ isActive }: { isActive: boolean }) => `px-4 py-6 inline-block text-xl font-bold ${isActive ? " text-primary border-b border-primary" : ""}`} to="/blog">
+        部落格
+      </NavLink>
     </nav>
   )
 }
