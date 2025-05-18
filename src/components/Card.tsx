32c1fdf25b66740682e5a3a16f4cfe8584733d0c
Benson <kumadth@gmail.com>
2025-05-18 14:38:47 +0800

    fix: 調整 Card 元件的程式碼格式，增強可讀性

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import { BlogPost } from "@/interfaces/blog";
+import { BlogPost } from '@/interfaces/blog';
 
 interface Badge {
   condition: boolean;
@@ -10,59 +10,69 @@ interface CardProps extends BlogPost {
 }
 
 const Card = (props: CardProps) => {
-  const { title, date, tags, description, imgUrl, isNew, isHot, aosDelay } = props;
+  const { title, date, tags, description, imgUrl, isNew, isHot, aosDelay } =
+    props;
 
   // 最新文章、人氣文章的條件 Map
   const additionalBadges: Badge[] = [
-    { condition: !!isHot, label: "最新文章" },
-    { condition: !!isNew, label: "人氣文章" },
+    { condition: !!isHot, label: '最新文章' },
+    { condition: !!isNew, label: '人氣文章' },
   ];
   const renderBadges = additionalBadges
     .filter((badge: Badge) => badge.condition)
-    .map((badge: Badge) => badge.label)
+    .map((badge: Badge) => badge.label);
 
   return (
-    <div 
-      data-aos="fade-up" 
-      data-aos-delay={aosDelay} 
-      data-aos-duration="500" 
-      className="hover:shadow-lg hover:-translate-y-2.5 transition-all duration-300 bg-white p-6"
+    <div
+      data-aos="fade-up"
+      data-aos-delay={aosDelay}
+      data-aos-duration="500"
+      data-aos-anchor-placement="center-bottom"
+      className="hover:shadow-lg hover:-translate-y-2.5 hover:transition-all duration-300 bg-white p-6"
     >
-      <img className="mb-4 max-h-[250px] w-full object-cover" src={imgUrl} alt={title} />
+      <img
+        className="mb-4 aspect-video object-cover"
+        src={imgUrl}
+        alt={title}
+      />
 
       <p className="mb-1">{date}</p>
 
       <div className="mb-2 flex flex-wrap items-center">
         {/* 渲染標籤 */}
-        {
-          tags.map((item, index) => {
-            return <span key={index} className="text-primary text-lg mr-2">
+        {tags.map((item, index) => {
+          return (
+            <span key={index} className="text-primary text-lg mr-2">
               {`#${item}`}
             </span>
-          })
-        }
+          );
+        })}
 
         {/* 渲染 Badge */}
-        {
-          renderBadges.map((badgeText, index) =>
-            <span key={index} className="bg-primary text-white py-1.5 px-3 ms-2 rounded-full font-bold">
-              {badgeText}
-            </span>
-          )
-        }
+        {renderBadges.map((badgeText, index) => (
+          <span
+            key={index}
+            className="bg-primary text-white py-1.5 px-3 ms-2 rounded-full font-bold"
+          >
+            {badgeText}
+          </span>
+        ))}
       </div>
 
       <h2 className="text-xl font-bold mb-2">{title}</h2>
 
-      <p className="line-clamp-2 mb-4">
-        {description}
-      </p>
+      <p className="line-clamp-2 mb-4">{description}</p>
 
       <div>
-        <a href="#" className="inline-block text-black py-2 px-3 border-black border-1 rounded-4xl">閱讀內文</a>
+        <a
+          href="#"
+          className="inline-block text-black py-2 px-3 border-black border-1 rounded-4xl"
+        >
+          閱讀內文
+        </a>
       </div>
     </div>
-  )
+  );
 };
 
 export default Card;
