3cd3462e46136f1308f1742e4692600b933e7d63
Benson <kumadth@gmail.com>
2025-05-19 00:20:22 +0800

    fix: 修正 Carousel 組件的 CSS 檔案引入路徑，清理不必要的註解

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,7 +1,7 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css'; // 核心樣式
 import 'swiper/css/navigation'; // 導航箭頭
-import '@/assets/swiper.css'; // 自訂樣式
+import '@/assets/style/swiper.css'; // 自訂樣式
 import Card from './Card';
 import blogPostList from '@/pages/Blog/blogPostList'; // 假資料：部落格文章列表
 
@@ -9,40 +9,27 @@ import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import { BlogPost } from '@/interfaces/blog';
 
 export default function Carousel() {
-  // const arrowZoneWidth = '60px';
 
   return (
-    <div
-    // style={{
-    //   position: 'relative',
-    //   paddingLeft: arrowZoneWidth,
-    //   paddingRight: arrowZoneWidth,
-    // }}
+    <Swiper
+      modules={[Navigation, Pagination, Autoplay]}
+      navigation // 導航箭頭
+      pagination={{ clickable: true }} // 顯示分頁圓點
+      autoplay={{ delay: 4000, disableOnInteraction: true }} // 自動播放
+      loop={true} // 循環
+      spaceBetween={24} // 幻燈片間距
+      slidesPerView={1} // 一次顯示 1 張
+      breakpoints={{
+        768: {
+          slidesPerView: 3, // 螢幕寬度大於 768px 時顯示 3 張
+        },
+      }}
     >
-      <Swiper
-        modules={[Navigation, Pagination, Autoplay]}
-        navigation // 導航箭頭
-        // navigation={{
-        //   nextEl: '.custom-swiper-button-next', // 指向自訂的下一個按鈕的 CSS 選擇器
-        //   prevEl: '.custom-swiper-button-prev', // 指向自訂的上一個按鈕的 CSS 選擇器
-        // }}
-        pagination={{ clickable: true }} // 顯示分頁圓點
-        // autoplay={{ delay: 4000, disableOnInteraction: false }} // 自動播放
-        loop={true} // 循環
-        spaceBetween={24} // 幻燈片間距
-        slidesPerView={1} // 一次顯示 1 張
-        breakpoints={{
-          768: {
-            slidesPerView: 3, // 螢幕寬度大於 768px 時顯示 3 張
-          },
-        }}
-      >
-        {blogPostList.map((blogPost: BlogPost) => (
-          <SwiperSlide key={blogPost.id}>
-            <Card {...blogPost} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+      {blogPostList.map((blogPost: BlogPost) => (
+        <SwiperSlide key={blogPost.id}>
+          <Card {...blogPost} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
