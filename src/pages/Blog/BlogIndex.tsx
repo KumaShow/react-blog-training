221d78a6671951425dba1fe54a36ced3a757ba0d
Benson <kumadth@gmail.com>
2025-05-19 00:22:06 +0800

    feat: 新增搜尋功能，改善文章過濾及顯示效果

--- src/pages/Blog/BlogIndex.tsx
+++ src/pages/Blog/BlogIndex.tsx
@@ -4,13 +4,24 @@ import Banner from '@/components/Banner';
 import { BlogPost } from '@/interfaces/blog';
 import blogPostList from '@/pages/Blog/blogPostList'; // 假資料：部落格文章列表
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
+import { useState } from 'react';
 
 export default function Blog() {
   // 假資料：最新文章
   const lastestBlogPost: BlogPost = blogPostList.filter(
     (blogPost: BlogPost) => blogPost.isNew
   )[0];
+
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
+  // 依據搜尋關鍵字過濾文章
+  const filteredBlogPosts = blogPostList
+    .filter((blogPost: BlogPost) => !blogPost.isNew)
+    .filter((blogPost: BlogPost) =>
+      blogPost.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
   return (
     <div>
@@ -46,7 +57,7 @@ export default function Blog() {
           <div>
             <Link
               to={`/blog/${lastestBlogPost.id}`}
-              className="inline-block text-black py-2 px-3 border-black border-1 rounded-4xl"
+              className="inline-block text-black py-2 px-3 border-black border-1 rounded-4xl hover:bg-primary hover:text-white transition duration-300"
             >
               閱讀內文
             </Link>
@@ -59,46 +70,63 @@ export default function Blog() {
         <div className="py-16  md:py-20 container ">
           <div className="md:px-3 md:grid md:grid-cols-12 md:gap-6">
             <div className="relative w-full md:w-auto md:col-span-4">
+              <FontAwesomeIcon
+                icon={faMagnifyingGlass}
+                className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400"
+              />
               <input
                 type="text"
                 className="block px-3 py-2 pl-10 rounded-4xl border-2 w-full"
                 placeholder="搜尋你感興趣的文章"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
               />
-              <FontAwesomeIcon
-                icon={faMagnifyingGlass}
-                className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
-              />
+
+              {searchTerm && (
+                <FontAwesomeIcon
+                  icon={faXmark}
+                  className="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-400 cursor-pointer"
+                  onClick={() => setSearchTerm('')}
+                />
+              )}
             </div>
           </div>
 
           <ul className="md:px- py-12 space-y-8 md:space-y-0 md:grid md:grid-cols-2 md:gap-6 lg:grid-cols-3">
-            {blogPostList
-              .filter((blogPost: BlogPost) => !blogPost.isNew)
-              .map((blogPost: BlogPost) => {
-                return (
-                  <li
-                    key={blogPost.id}
-                    data-aos="fade-up"
-                    data-aos-delay={500}
-                    data-aos-duration="500"
-                    data-aos-anchor-placement="center-bottom"
-                  >
-                    <Card {...blogPost} />
-                  </li>
-                );
-              })}
+            {filteredBlogPosts.map((blogPost: BlogPost, index: number) => {
+              return (
+                <li
+                  key={blogPost.id}
+                  data-aos="fade-up"
+                  data-aos-delay={index * 50}
+                  data-aos-duration="800"
+                  data-aos-anchor-placement="center-bottom"
+                >
+                  <Card {...blogPost} />
+                </li>
+              );
+            })}
           </ul>
 
+          {/* 查無資料 */}
+          {filteredBlogPosts.length === 0 && (
+            <div className="text-center text-2xl text-gray-500">
+              查無相關文章
+            </div>
+          )}
+
           {/* Pagination */}
-          <div className="flex justify-center mt-8">
-            <button className="bg-transparent px-2">{`<`}</button>
-            {Array.from({ length: 5 }, (_, index) => (
-              <button key={index} className="bg-transparent py-2.5 px-4">
-                {index + 1}
-              </button>
-            ))}
-            <button className="bg-transparent ms-4 px-2">{`>`}</button>
-          </div>
+          {filteredBlogPosts.length > 0 && (
+            <div className="flex justify-center mt-8 *:hover:text-primary *:hover:-translate-y-[2px] *:transition *:duration-200">
+              <button className="bg-transparent px-2">{`<`}</button>
+              {Array.from({ length: 5 }, (_, index) => (
+                <button key={index} className="bg-transparent py-2.5 px-4">
+                  {index + 1}
+                </button>
+              ))}
+              <button className="bg-transparent ms-4 px-2">{`>`}</button>
+            </div>
+          )}
         </div>
       </section>
     </div>
